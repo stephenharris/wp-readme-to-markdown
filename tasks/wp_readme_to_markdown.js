2e9c183a867002d1b5b609d875e229b6113459b4
pedro-mendonca <ped.gaspar@gmail.com>
2023-07-20 14:37:53 +0100

    Undo forcing CRLF on screenshots

--- tasks/wp_readme_to_markdown.js
+++ tasks/wp_readme_to_markdown.js
@@ -112,11 +112,11 @@ module.exports = function(grunt) {
 				var url = options.screenshot_url;
 				url = url.replace( '{plugin}', plugin );
 				url = url.replace( '{screenshot}', 'screenshot-'+i );
-				var ending = "\r\n";
+				var ending = "\n";
 				if ( i === matchArray.length ) {
 					ending = "";
 				}
-				readme = readme.replace(  globalMatch[i-1], "### "+i+". "+ matchArray[i-1] +" ###\r\n!["+matchArray[i-1]+"](" + url + ")" + ending );
+				readme = readme.replace(  globalMatch[i-1], "### "+i+". "+ matchArray[i-1] +" ###\n!["+matchArray[i-1]+"](" + url + ")" + ending );
 			}
 		}
 
